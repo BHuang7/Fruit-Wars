e3772de3352e5240b53f87bb37f30d8b49a0e2a8
Brian Huang <brianhuangoff@gmail.com>
2020-01-31 10:46:03 -0800

    Added a collider onto the banana, doesn't collide with anything yet

--- Ban.js
+++ Ban.js
@@ -1,7 +1,10 @@
 function ban(game, spritesheetArray) {
+	this.scalingFactor = 1.1;
 	this.animation = new arrAnimation(spritesheetArray, .05, true, 1.5);
     this.speed = 250;
     this.ctx = game.ctx;
+	this.height = spritesheetArray[0].height;
+	this.width = spritesheetArray[0].width;
     Entity.call(this, game, 0, 50);
 }
 
@@ -15,6 +18,15 @@ ban.prototype.update = function () {
 }
 
 ban.prototype.draw = function () {
+	this.ctx.fillStyle = 'rgba(255, 255, 255, 255)'
+	this.ctx.beginPath();
+	this.ctx.arc(this.x + (this.calculateBoundingCircleRadius() * this.scalingFactor), this.y + (this.calculateBoundingCircleRadius() * this.scalingFactor), this.calculateBoundingCircleRadius() * this.scalingFactor, 0, 2 * Math.PI, false);
+	this.ctx.fill();
     this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
     Entity.prototype.draw.call(this);
+}
+
+ban.prototype.calculateBoundingCircleRadius = function() {
+	
+	return Math.sqrt(((this.width/2 * this.width/2) + (this.height/2 * this.height/2)));
 }
