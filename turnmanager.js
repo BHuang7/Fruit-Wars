e809b5f9cde7347518093bd3de8b1e0bd5fd3487
Brian Huang <brianhuangoff@gmail.com>
2020-02-18 09:25:51 -0800

    built switching between players framework

--- turnmanager.js
+++ turnmanager.js
@@ -5,6 +5,7 @@ function turnManager(gameEngine) {
 	this.playerFour = {player: 4, hp :100, turn: false};
 	this.timer = 30;
 	this.starting = true;
+	this.shot = false;
 	var ground = new Terrain(gameEngine);
 	ground.coordinates = ground.generate(50, 500, 50);	
 	gameEngine.addEntity(new Background(gameEngine));
@@ -13,6 +14,7 @@ function turnManager(gameEngine) {
 	gameEngine.addEntity(new ban(gameEngine, ground, this, this.playerOne));
 	gameEngine.addEntity(new lime(gameEngine, ground, this, this.playerTwo));
 	gameEngine.addEntity(new pineapple(gameEngine, ground, this, this.playerThree));
+	this.turns = [this.playerOne, this.playerTwo, this.playerThree, this.playerFour];
 }
 
 
@@ -24,6 +26,28 @@ turnManager.prototype.update = function () {
 	if (this.starting) {
 		this.playerOne.turn = true;
 		this.starting = false;
+	}
+	if (this.shot) {
+		if (this.playerOne.turn) {
+			this.playerOne.turn = false;
+			this.playerThree.turn = true;
+			this.shot = false;
+		}
+		else if (this.playerThree.turn) {
+			this.playerThree.turn = false;
+			this.playerTwo.turn = true;
+			this.shot = false;
+		}
+		else if (this.playerTwo.turn) {
+			this.playerTwo.turn = false;
+			this.playerFour.turn = true;
+			this.shot = false;
+		}
+		else {
+			this.playerFour.turn = false;
+			this.playerOne.turn = true;
+			this.shot = false;
+		}
 	}
 	Entity.prototype.update.call(this);
 }
